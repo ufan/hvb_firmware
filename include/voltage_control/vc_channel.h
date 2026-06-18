18ff9ce563a6db7725215a13c640ee2c249ba657
Yong Zhou <zyong06@gmail.com>
2026-06-18 19:28:19 +0800

    feat: add shared vc_channel_api header

---
+++ include/voltage_control/vc_channel.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2026 Jianwei
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef VOLTAGE_CONTROL_VC_CHANNEL_H
+#define VOLTAGE_CONTROL_VC_CHANNEL_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <zephyr/device.h>
+
+struct vc_channel_api {
+	int (*set_output)(const struct device *dev, uint16_t code);
+	int (*set_enable)(const struct device *dev, bool enable);
+	int (*measure_voltage)(const struct device *dev, int32_t *value);
+	int (*measure_current)(const struct device *dev, int32_t *value);
+	uint16_t (*get_capabilities)(const struct device *dev);
+};
+
+#endif
