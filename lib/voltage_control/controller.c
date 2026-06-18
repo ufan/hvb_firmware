f61533f29677322bcb443cfc8f2677b75f5c978c
Yong Zhou <zyong06@gmail.com>
2026-06-18 19:29:21 +0800

    feat: add VC controller aggregation and HVB channel binding

---
+++ lib/voltage_control/controller.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2026 Jianwei
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Compiles only when DTS vc_controller node exists (Kconfig guard).
+ */
+
+#include <zephyr/devicetree.h>
+#include "voltage_control/vc_channel.h"
+
+#define VC_CONTROLLER_NODE DT_NODELABEL(vc_controller)
+
+#define CH_ENTRY(node_id, prop, idx) \
+	[idx] = { \
+		.dev = DEVICE_DT_GET(DT_PHANDLE_BY_IDX(node_id, prop, idx)), \
+		.index = DT_PROP(DT_PHANDLE_BY_IDX(node_id, prop, idx), \
+				 channel_index), \
+		.capabilities = DT_PROP(DT_PHANDLE_BY_IDX(node_id, prop, idx), \
+					 capabilities), \
+	},
+
+const struct vc_channel_entry {
+	const struct device *dev;
+	uint8_t            index;
+	uint16_t           capabilities;
+} vc_domain_channels[] = {
+	DT_FOREACH_PROP_ELEM(VC_CONTROLLER_NODE, channels, CH_ENTRY)
+};
+
+const size_t vc_domain_channel_count = ARRAY_SIZE(vc_domain_channels);
