544cac9a83fde821b0c4d369f3c9fce1b4665411
Yong Zhou <zyong06@gmail.com>
2026-07-15 18:56:40 +0800

    docs(voltage_control): document CH_CAP_* capability flag meanings
    Add a one-line description above each channel-capability bitmask so
    readers don't have to cross-reference vc_catalog_supported() to know
    what each flag gates.

--- include/dt-bindings/voltage_control/capabilities.h
+++ include/dt-bindings/voltage_control/capabilities.h
@@ -5,10 +5,15 @@
 #ifndef DT_BINDINGS_VOLTAGE_CONTROL_CAPABILITIES_H
 #define DT_BINDINGS_VOLTAGE_CONTROL_CAPABILITIES_H
 
+/* Indicates that the channel can be enabled for output operation, i.e. on/off. */
 #define CH_CAP_OUTPUT_ENABLE           0x0001
+/* Indicates that the channel can drive a raw output signal, i.e. DAC output */
 #define CH_CAP_RAW_OUTPUT_DRIVE        0x0002
+/* Indicates that the channel supports voltage measurement, i.e. ADC input for voltage sensing. */
 #define CH_CAP_VOLTAGE_MEASUREMENT     0x0004
+/* Indicates that the channel supports current measurement, i.e. ADC input for current sensing. */
 #define CH_CAP_CURRENT_MEASUREMENT     0x0008
+/* Indicates that the channel exposes hardware status information. */
 #define CH_CAP_HARDWARE_STATUS         0x0010
 
 #endif /* DT_BINDINGS_VOLTAGE_CONTROL_CAPABILITIES_H */
